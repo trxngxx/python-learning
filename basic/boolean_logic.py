8981c64e2bc9fc3857a87054422e3f3a0f94736e
trxngxx <nnartnav@gmail.com>
2024-08-12 23:35:04 +0700

    update

---
+++ basic/boolean_logic.py
@@ -0,0 +1,13 @@
+print("Boolean logic")
+a = input("a: ")
+b = input("b: ")
+# test expression
+if a < b:
+    # statement to be run
+    print(b)
+
+if a > b:
+    print(a)
+
+elif a == b:
+    print("a and b are equal")
