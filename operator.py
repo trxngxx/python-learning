a3ee1f743845b671e15acd9415d44d35582d7252
trxngxx <nnartnav@gmail.com>
2024-08-12 22:41:45 +0700

    update

---
+++ operator.py
@@ -0,0 +1,5 @@
+left_side = 10
+right_side = 5
+allocation = left_side / right_side 
+
+print("allocation: ", allocation)
--- sum.py
+++ sum.py
@@ -1,4 +1,3 @@
-planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
-distance_to_alpha_centauri = 4.367 # float, lightyears
-can_liftoff = True
-shuttle_landed_on_the_moon = "Apollo 11" #string
+sum = 1 + 541451143
+product = sum * 2
+print(product)
