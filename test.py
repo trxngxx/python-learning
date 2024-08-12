16851f3bec4d187e4795bbf5bdc9f05062e35abc
trxngxx <nnartnav@gmail.com>
2024-08-12 22:40:04 +0700

    update

--- test.py
+++ test.py
@@ -2,3 +2,10 @@ planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too 
 distance_to_alpha_centauri = 4.367 # float, lightyears
 can_liftoff = True
 shuttle_landed_on_the_moon = "Apollo 11" #string
+
+
+
+print(planets_in_solar_system);\
+print(distance_to_alpha_centauri);\
+print(can_liftoff);\
+print(shuttle_landed_on_the_moon)
