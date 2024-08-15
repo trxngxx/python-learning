8bf4cccc851528089072044ef4d67e4e307487f1
trxngxx <nnartnav@gmail.com>
2024-08-15 22:03:50 +0700

    update

---
+++ basic/string.py
@@ -0,0 +1,12 @@
+# print("temperatures and facts about the moon".title())
+
+# heading = "temperatures and facts about the moon"
+# heading_upper = heading.title()
+# print(heading_upper)
+# print(heading.title()) 
+
+# temperatures = "Daylight: 260 F\n Nighttime: -280 F"
+# temperatures_list = temperatures.split('\n')
+# print(temperatures_list)
+
+print("Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius.".replace("Celsius", "C"))
