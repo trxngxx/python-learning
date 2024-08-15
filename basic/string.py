5959be33f7998d5047339c3a6e07bca254b74f16
trxngxx <nnartnav@gmail.com>
2024-08-15 22:06:58 +0700

    update

--- basic/string.py
+++ basic/string.py
@@ -10,3 +10,6 @@
 # print(temperatures_list)
 
 print("Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius.".replace("Celsius", "C"))
+
+moon_facts = ["The Moon is drifting away from the Earth.", "On average, the Moon is moving about 4cm every year."]
+print(' '.join(moon_facts))
