0f3b438fb2fb9a2aef336150b85c6139c0fc1836
trxngxx <nnartnav@gmail.com>
2024-08-15 23:51:32 +0700

    update

--- basic/string.py
+++ basic/string.py
@@ -1,15 +1,22 @@
-# print("temperatures and facts about the moon".title())
+print("temperatures and facts about the moon".title())
 
-# heading = "temperatures and facts about the moon"
-# heading_upper = heading.title()
-# print(heading_upper)
-# print(heading.title()) 
+heading = "temperatures and facts about the moon"
+heading_upper = heading.title()
+print(heading_upper)
+print(heading.title()) 
 
-# temperatures = "Daylight: 260 F\n Nighttime: -280 F"
-# temperatures_list = temperatures.split('\n')
-# print(temperatures_list)
+temperatures = "Daylight: 260 F\n Nighttime: -280 F"
+temperatures_list = temperatures.split('\n')
+print(temperatures_list)
 
 print("Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius.".replace("Celsius", "C"))
 
 moon_facts = ["The Moon is drifting away from the Earth.", "On average, the Moon is moving about 4cm every year."]
 print(' '.join(moon_facts))
+
+mass_percentage = "1/8"
+print("On the Moon, you would weigh about %s of your weight on Earth." % mass_percentage)
+
+
+mass_percentage = "1/6"
+print("""You are lighter on the {0}, because on the {0} you would weigh about {1} of your weight on Earth.""".format("Moon", mass_percentage))
